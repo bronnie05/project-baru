227b89329f594f4c1986d9b8741d3add0c73df9c
bronnie_ <mfarhanz375@gmail.com>
2026-09-05 16:10:29 +0700

    nambah if else

--- kalkulator_ibm.py
+++ kalkulator_ibm.py
@@ -10,6 +10,15 @@ tinggi_badan = float(tinggi_badan)
 
 bmi = berat_badan/(tinggi_badan**2)
 
+if bmi < 18.5:
+    kategori = "kekurangan berat badan"
+elif bmi < 25:
+    kategori = "normal"
+elif bmi < 30:
+    kategori = "kelebihan berat badan"
+else:
+    kategori = "obesitas"
+
 berat_badan_ideal = dict()
 berat_badan_ideal["bawah"] = 18.5 * (tinggi_badan**2)
 berat_badan_ideal["atas"] = 24.9 * (tinggi_badan**2)
@@ -18,4 +27,4 @@ print(berat_badan_ideal)
 
 print(f"nilai bmi anda adalah: {bmi:.2f}")
 print("berat badan ideal adalah 18,5-24.9 kg/m2")
-print(f"berat badan ideal anda adalah: {berat_badan_ideal['bawah']:.2f} - {berat_badan_ideal['atas']:.2f} kg")
+print(f"kategori: {kategori}")
